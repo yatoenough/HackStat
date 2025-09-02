37ce9dd5f251f9ad491acd4ed882d5535c8eaaa4
Nikita Shyshkin <yatoenough@gmail.com>
2025-09-02 20:48:22 +0200

    Init app

---
+++ HackStat/HackStatApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackStatApp.swift
+//  HackStat
+//
+//  Created by Nikita Shyshkin on 02/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct HackStatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
